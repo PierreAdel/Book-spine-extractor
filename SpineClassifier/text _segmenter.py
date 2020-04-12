11be3230f81a74a50d18b777ed1f4863c6f14836
Ramez Noshy <RamezNoshy@gmail.com>
2020-04-12 15:09:13 +0200

    add basic character recognition

--- SpineClassifier/text _segmenter.py
+++ SpineClassifier/text _segmenter.py
@@ -1,4 +1,11 @@
 import cv2
+import pytesseract
+
+IMAGE_PATH = 'images/'
+NORMAL_TEXT_PATH = IMAGE_PATH + 'normal_text/'
+SHELVES_PATH = IMAGE_PATH + 'normal_text/'
+SPINES_PATH = IMAGE_PATH + 'spines/'
+
 
 class TextSegmenter:
 
@@ -8,7 +15,10 @@ class TextSegmenter:
         :return:
             list of words
         """
-        pass
+        binary_image = self.segment_text(image)
+        cv2.imshow('binary_image', binary_image)
+        cv2.waitKey()
+        return self.recognize_text(binary_image)
 
     def segment_text(self, image):
         """
@@ -16,7 +26,27 @@ class TextSegmenter:
         :return:
             binarized image
         """
-        pass
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('gray', gray)
+        cv2.waitKey()
+
+        fun = lambda im: cv2.threshold(im, 0, 255,
+                             cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        b = image[:, :, 0]
+        cv2.imshow('b', b)
+        cv2.imshow('b_thresh', fun(b))
+
+        g = image[:, :, 1]
+        cv2.imshow('g', b)
+        cv2.imshow('g_thresh', fun(g))
+
+        r = image[:, :, 2]
+        cv2.imshow('r', r)
+        cv2.imshow('r_thresh', fun(r))
+        cv2.waitKey()
+
+        return cv2.threshold(gray, 0, 255,
+                             cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
     def recognize_text(self, binary_image):
         """
@@ -24,8 +54,12 @@ class TextSegmenter:
         :return:
             text included in image
         """
-        pass
+        return pytesseract.image_to_string(binary_image)
 
 
 if __name__ == "__main__":
-    pass
+
+    img = cv2.imread(SPINES_PATH + '6.jpg')
+    img = cv2.medianBlur(img, 5)
+    segmenter = TextSegmenter()
+    print(segmenter.get_text(img))
