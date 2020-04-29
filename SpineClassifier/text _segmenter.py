47e590be9759934f333caeab61319211530ca80c
Ramez <rameznoshy@gmail.com>
2020-04-30 00:29:26 +0200

    add goodreads integration, clean code

--- SpineClassifier/text _segmenter.py
+++ SpineClassifier/text _segmenter.py
@@ -13,7 +13,6 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 class BoundingBoxWrapper:
     def __init__(self, stats, img_shape):
-        # self.locations = {}
         self.boxes = []
         self.add_boxes(img_shape, stats)
 
@@ -23,12 +22,6 @@ class BoundingBoxWrapper:
             # todo do multilevel nesting and optimize method
 
             if box.is_valid(img_shape):
-                # for level, box_list in self.locations:
-                #     for stored_box in box_list:
-                #         if box.inside(stored_box):
-                #             self.locations[level].append(box)
-                #             break
-
                 inside = any(box.inside(stored_box) for stored_box in self.boxes)
                 if inside:
                     continue
@@ -98,100 +91,33 @@ class TextSegmenter:
         self.image = resize_image(im)
         self.image_area = im.shape[0] * im.shape[1]
         self.gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-        # self.gray_chen = np.ones(self.gray.shape, dtype=np.uint8) * 255
-        # self.gray_otsu = np.ones(self.gray.shape, dtype=np.uint8) * 255
         self.gray_chen = np.zeros(self.gray.shape, dtype=np.uint8) * 255
         self.gray_otsu = np.zeros(self.gray.shape, dtype=np.uint8) * 255
-        # cv2.imshow('sssss', self.gray_otsu)
-        # cv2.waitKey()
-        self.segmented_img = self.segment_text()
-        cv2.imshow('binary_image', self.segmented_img)
-        # cv2.waitKey()
+        self.get_character_edges(self.image)
+        # self.segmented_img = self.segment_text()
+        # cv2.imshow('binary_image', self.segmented_img)
         self.text = self.recognize_text()
-
-    def preprocess_image(self, image):
-        image = resize_image(image)
-        kernel = np.ones((5, 5), np.uint8)
-        image = cv2.medianBlur(image, 3)
-        # opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-        # closing = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-        return image
 
     def get_text(self):
         return self.text
-
-    def segment_text(self):
-        """
-
-        :return:
-            binarized image
-        """
-        cv2.imshow('gray', self.gray)
-        processed = self.preprocess_image(self.gray)
-        cv2.imshow('processed', processed)
-
-        edges = self.get_character_edges(self.image)
-
-        return cv2.threshold(self.gray, 0, 255,
-                             cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
     def get_character_edges(self, image):
         b = image[:, :, 0]
         g = image[:, :, 1]
         r = image[:, :, 2]
         edges = thresh_edges(b) | thresh_edges(r) | thresh_edges(g)
-        cv2.imshow('edges', edges)
-        cv2.imshow('edgesr', thresh_edges(r))
-        cv2.imshow('edgesg', thresh_edges(g))
-        cv2.imshow('edgesb', thresh_edges(b))
         edges2 = thresh_edges(b) / 3 + thresh_edges(r) / 3 + thresh_edges(g) / 3
-        cv2.imshow('edges2', edges2)
         edges2 = edges2.astype('uint8')
         edges3 = cv2.threshold(edges2, 160, 255,
                                cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        cv2.imshow('edges3', edges3)
-        # Taking a matrix of size 5 as the kernel
+
         kernel = np.ones((2, 2), np.uint8)
         edgesmorph = cv2.morphologyEx(edges3, cv2.MORPH_CLOSE, kernel)
-        cv2.imshow('edgesmorph', edgesmorph)
         edgeblur = cv2.GaussianBlur(edges2, (3, 3), 3)
         edgeblur = cv2.threshold(edgeblur, 220, 255,
                                  cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        cv2.imshow('edgeblur', edgeblur)
-        # edges = cv2.threshold(cv2.Canny(edges, 20, 80), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        # cv2.imshow('edges2', edges)
         edges3 = edges
         num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(edges3, 8, cv2.CV_32S)
-
-        # for index, (left, up, wid, height, area) in enumerate(stats):
-        #     if wid * height > self.image_area * 0.15:
-        #         continue
-        #     elif 0.2 < wid / height < 5 and max(height, wid) > 0.05 * image.shape[0]:
-        #
-        #         ROI = labels[up:up + height, left:left + wid]
-        #         edge_mask_of_label = (ROI == index)
-        #         true_count = np.count_nonzero(edge_mask_of_label)
-        #         gray_roi = self.gray[up:up + height, left:left + wid]
-        #         labeled_gray_roi = edge_mask_of_label * gray_roi
-        #         labeled_mean_intensity = np.sum(labeled_gray_roi) / true_count
-        #         # mean_intensity = np.mean(gray_roi)
-        #         # mean_border = np.mean(np.concatenate([gray_roi[0], gray_roi[:, 0]]))
-        #         # invert = False if mean_border < labeled_mean_intensity else True
-        #         # variance = np.var(labeled_gray_roi)
-        #         # standard_deviation = np.sqrt(variance * labeled_gray_roi.size / true_count)
-        #         # if standard_deviation < 30:
-        #         #     continue
-        #         cv2.rectangle(image, (left, up), (left + wid, up + height), (0, 255, 0), 1)
-        #         # cv2.imshow('img', image)
-        #         # cv2.waitKey()
-        #         self.gray_chen[up:up + height, left:left + wid] = cv2.threshold(gray_roi, labeled_mean_intensity, 255,
-        #                                                                         cv2.THRESH_BINARY)[1]
-        #
-        #         self.gray_otsu[up:up + height, left:left + wid] = cv2.threshold(gray_roi, 0, 255,
-        #                                                                         cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        #         # if invert:
-        #         #     self.gray_chen[up:up + height, left:left + wid] = \
-        #         #         cv2.bitwise_not(self.gray_chen[up:up + height, left:left + wid])
 
         box_wrapper = BoundingBoxWrapper(stats, self.image.shape)
         for box in box_wrapper.get_boxes():
@@ -219,27 +145,13 @@ class TextSegmenter:
             if mean_inside < mean_outside:
                 self.gray_otsu[box.up:box.bottom, box.left:box.right] = \
                     255 - self.gray_otsu[box.up:box.bottom, box.left:box.right]
-            # print(f"inside is {mean_inside}, outside is {mean_outside}")
-            cv2.imshow('edges_bounded', image)
-            # cv2.waitKey()
-            # average_border_mean = np.mean(self.gray_otsu[box.up:box.up + 1, box.left:box.right])
-            # average_border_mean += np.mean(self.gray_otsu[box.bottom:box.bottom + 1, box.left:box.right])
-            # average_border_mean += np.mean(self.gray_otsu[box.up:box.bottom, box.left:box.left + 1])
-            # average_border_mean += np.mean(self.gray_otsu[box.up:box.bottom, box.right:box.right + 1])
-            # average_border_mean /= 4
-            # if average_border_mean > 60:
-            #     self.gray_otsu[box.up:box.bottom, box.left:box.right] = \
-            #         255 - self.gray_otsu[box.up:box.bottom, box.left:box.right]
 
-        # self.gray_otsu = 255 - self.gray_otsu
         self.gray_chen = cv2.medianBlur(self.gray_chen, 3)
         self.gray_otsu = cv2.medianBlur(self.gray_otsu, 3)
         # self.gray_otsu = cv2.GaussianBlur(self.gray_otsu, (3, 3), 3)
-        cv2.imshow('gray_chen', self.gray_chen)
-        cv2.imshow('gray_otsu', self.gray_otsu)
-        cv2.imshow('edges_bounded', image)
-        cv2.waitKey()
-        return edges
+        # cv2.imshow('gray_otsu', self.gray_otsu)
+        # cv2.imshow('edges_bounded', image)
+        # cv2.waitKey()
 
     def recognize_text(self):
         """
@@ -248,9 +160,10 @@ class TextSegmenter:
             text included in image
         """
         config = '-c tessedit_char_whitelist=\ 01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-        return "chen *************\n" + pytesseract.image_to_string(cv2.bitwise_not(self.gray_chen)) + \
-               "\notsu *************\n" + pytesseract.image_to_string(cv2.bitwise_not(self.gray_otsu), lang='eng',
-                                                                      config=config)
+        text = pytesseract.image_to_string(cv2.bitwise_not(self.gray_otsu), lang='eng', config=config)
+        return text
+        # return "chen *************\n" + pytesseract.image_to_string(cv2.bitwise_not(self.gray_chen)) + \
+        #        "\notsu *************\n" + text
 
 
 def resize_image(im):
@@ -274,17 +187,57 @@ def thresh_edges(im):
                          cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 
+# todo fix bounding boxes
+#   todo mawdoo3 el inner/ nesting
+# todo fix inverting the colors if necessary
+# todo check the mode of the pixel above the letter -- done
+# todo sort boxes by size before detecting if its inside another one -- done
+# todo idea: fill connected component -- cancelled
+# todo idea: increase contrast
+# todo combine boxes?
+
+from googlesearch import search
+from goodreads import client
+import re
+from dotenv import load_dotenv
+load_dotenv()
+import os
+KEY = os.getenv("goodreads_key")
+gc = client.GoodreadsClient(KEY, "")
 if __name__ == "__main__":
+
+    gamma = 0.5
+    inverse_gamma = 1 / gamma
+
+    look_up_table = np.zeros(256, dtype="uint8")
+
     for i in range(17):
         cv2.destroyAllWindows()
         img = cv2.imread(SPINES_PATH + str(i) + '.jpg')
         segmenter = TextSegmenter(img)
-        print(segmenter.get_text())
-
-# todo fix bounding boxes
-#   todo mawdoo3 el inner/ nesting
-# todo fix inverting the colors if necessary
-# todo check the mode of the pixel above and below letter
-# todo sort boxes by size before detecting if its inside another one
-# todo idea: fill connected component
-# todo combine boxes?
+        detected_text = segmenter.get_text()
+        try:
+            try:
+                book = gc.search_books(detected_text)[0]
+            except TypeError:
+                book = None
+            goodreads_url_prefix = "https://www.goodreads.com/book/show/"
+            if book is None:
+                gen = search(detected_text + " " + goodreads_url_prefix, tld='com', lang='en', num=1,
+                             pause=0.25)
+                site = next(gen)
+                if goodreads_url_prefix not in site:
+                    site = next(search(detected_text + " site:" + goodreads_url_prefix, tld='com', lang='en',
+                                       num=1, pause=0.25))
+                id = re.search("[0-9]+", site).group()
+                book = gc.book(id)
+            book_json = {
+                'title': book.title,
+                'author': book.authors[0].name,
+                'average_rating': book.average_rating,
+                'url': goodreads_url_prefix + book.gid
+            }
+            print(book_json)
+        except StopIteration:
+            site = f'not found \n, query="{detected_text}"'
+            print(site)
