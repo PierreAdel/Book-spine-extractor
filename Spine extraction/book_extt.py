1137e54c8d33d57e2f24e335ef4ef0e3381e4a2f
PierreAdel <38634825+PierreAdel@users.noreply.github.com>
2020-05-03 01:37:05 +0200

    better approach used

--- Spine extraction/book_extt.py
+++ Spine extraction/book_extt.py
@@ -1,284 +1,236 @@
-import cv2
+# import the necessary packages
+from imutils.object_detection import non_max_suppression
 import numpy as np
-import math
+import time
+import cv2
+import statistics
+
+# load the input image and grab the image dimensions
+image = cv2.imread("a.jpeg",)
+blackimage = cv2.imread("BLACK.png")
+
+disize = (1440,1080)
 
 
-img = cv2.imread('a (2).jpg', cv2.IMREAD_COLOR)
-imgclear = img
-black = cv2.imread('BLACK.png', cv2.IMREAD_COLOR)
+image = cv2.resize(image,disize)
 
-b1 = cv2.imread('BLACK.png', cv2.IMREAD_COLOR)
-
-imgsmooth = cv2.GaussianBlur(img, (5, 5), 0)
-
-hsv = cv2.cvtColor(imgsmooth, cv2.COLOR_BGR2HSV)
-
-
-def filter_by_color(black):
-
-    if black == True:
-        image = b1
-    else:
-        image = img
-
-    lower_red = np.array([160, 30, 70])
-    upper_red = np.array([190, 255, 255])
-    maskred = cv2.inRange(hsv, lower_red, upper_red)
-
-    lower_green = np.array([40, 40, 40])
-    upper_green = np.array([70, 255, 255])
-    maskgreen = cv2.inRange(hsv, lower_green, upper_green)
-
-    lower_blue = np.array([101, 50, 38])
-    upper_blue = np.array([130, 255, 255])
-    maskblue = cv2.inRange(hsv, lower_blue, upper_blue)
-
-    lower_yellow = np.array([22, 93, 0])
-    upper_yellow = np.array([45, 255, 255])
-    maskyellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
-
-
-    lower_brown = np.array([10,100,200])
-    upper_brown = np.array([20, 255, 200])
-    maskbrown = cv2.inRange(hsv, lower_brown, upper_brown)
-
-
-
-    resultred = cv2.bitwise_and(img, img, mask=maskred)
-    resultgreen = cv2.bitwise_and(img, img, mask=maskgreen)
-    resultblue = cv2.bitwise_and(img, img, mask=maskblue)
-    resultyellow = cv2.bitwise_and(img, img, mask=maskyellow)
-    resultbrown = cv2.bitwise_and(img, img, mask=maskbrown)
-
-
-    edgesred = cv2.Canny(resultred, 10, 250)
-    kernelred = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    closedred = cv2.morphologyEx(edgesred, cv2.MORPH_CLOSE, kernelred)
-    contoursred, hierarchy = cv2.findContours(closedred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    edgesgreen = cv2.Canny(resultgreen, 10, 250)
-    kernelgreen = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    closedgreen = cv2.morphologyEx(edgesgreen, cv2.MORPH_CLOSE, kernelgreen)
-    contoursgreen, hierarchy = cv2.findContours(closedgreen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    edgesblue = cv2.Canny(resultblue, 10, 250)
-    kernelblue = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    closedblue = cv2.morphologyEx(edgesblue, cv2.MORPH_CLOSE, kernelblue)
-    contoursblue, hierarchy = cv2.findContours(closedblue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    edgesyellow = cv2.Canny(resultyellow, 10, 250)
-    kernelyellow = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    closedyellow = cv2.morphologyEx(edgesyellow, cv2.MORPH_CLOSE, kernelyellow)
-    contoursyellow, hierarchy = cv2.findContours(closedyellow, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    edgesbrown = cv2.Canny(resultbrown, 10, 250)
-    kernelbrown = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    closedbrown = cv2.morphologyEx(edgesbrown, cv2.MORPH_CLOSE, kernelbrown)
-    contoursbrown, hierarchy = cv2.findContours(closedbrown, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    contourshere = [contoursred ,contoursgreen, contoursblue, contoursyellow, contoursbrown]
-
-    dims = img.shape
-    print("width", dims)
-
-    height = img.shape[0]
-    width = img.shape[1]
-    channels = img.shape[2]
-
-    for contourCOLOR in contourshere:
-        for c in contourCOLOR:
-            area = cv2.contourArea(c)
-            x, y, w, h = cv2.boundingRect(c)
-
-            ratio = w / h
-            print("ratio= "+str(ratio))
-
-            if area >= 3000 and ratio > 0.01 and ratio < 0.4:
-
-                cv2.drawContours(image, c, -1, (160, 30, 70), 3)
-                x, y, w, h = cv2.boundingRect(c)
-                cv2.rectangle(image, (x, y-200), (x + w, y + h), (160, 130, 70), 4)
-
-                center = (x, y)
-
-                cropped = imgclear[y:y + h, x:x + w]
-
-
-              #  cv2.imshow("cropped", cropped)
-               # cv2.imwrite( str(x)+".png", cropped)
-               # cv2.waitKey()
-
-    cv2.imshow("color",image)
-    cv2.waitKey()
+image = cv2.GaussianBlur(image, (3, 3), 0)
 
 
 
 
+orig = image
+width = 32 *45
+height = 32 *24
+min_confi = 0.1
+
+H, W, C = image.shape
 
 
 
+# set the new width and height and then determine the ratio in change
+# for both the width and height
+(newW, newH) = (width, height)
+rW = W / float(newW)
+rH = H / float(newH)
 
-# Convert the image to gray-scale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# resize the image and grab the new image dimensions
+image = cv2.resize(image, (newW, newH))
+(H, W) = image.shape[:2]
 
-edges = cv2.Canny(gray, 10, 250)
-cv2.imshow("canny", gray)
+# define the two output layer names for the EAST detector model that
+# we are interested -- the first is the output probabilities and the
+# second can be used to derive the bounding box coordinates of text
+layerNames = [
+	"feature_fusion/Conv_7/Sigmoid",
+	"feature_fusion/concat_3"]
+
+# load the pre-trained EAST text detector
+print("[INFO] loading EAST text detector...")
+net = cv2.dnn.readNet("frozen_east_text_detection.pb")
+
+# construct a blob from the image and then perform a forward pass of
+# the model to obtain the two output layer sets
+blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+	(123.68, 116.78, 103.94), swapRB=True, crop=False)
+start = time.time()
+net.setInput(blob)
+(scores, geometry) = net.forward(layerNames)
+end = time.time()
+
+# show timing information on text prediction
+print("[INFO] text detection took {:.6f} seconds".format(end - start))
+
+# grab the number of rows and columns from the scores volume, then
+# initialize our set of bounding box rectangles and corresponding
+# confidence scores
+(numRows, numCols) = scores.shape[2:4]
+rects = []
+confidences = []
+
+# loop over the number of rows
+for y in range(0, numRows):
+	# extract the scores (probabilities), followed by the geometrical
+	# data used to derive potential bounding box coordinates that
+	# surround text
+	scoresData = scores[0, 0, y]
+	xData0 = geometry[0, 0, y]
+	xData1 = geometry[0, 1, y]
+	xData2 = geometry[0, 2, y]
+	xData3 = geometry[0, 3, y]
+	anglesData = geometry[0, 4, y]
+
+	# loop over the number of columns
+	for x in range(0, numCols):
+		# if our score does not have sufficient probability, ignore it
+		if scoresData[x] < min_confi:
+			continue
+
+		# compute the offset factor as our resulting feature maps will
+		# be 4x smaller than the input image
+		(offsetX, offsetY) = (x * 4.0, y * 4.0)
+
+		# extract the rotation angle for the prediction and then
+		# compute the sin and cosine
+		angle = anglesData[x]
+		cos = np.cos(angle)
+		sin = np.sin(angle)
+
+		# use the geometry volume to derive the width and height of
+		# the bounding box
+		h = xData0[x] + xData2[x]
+		w = xData1[x] + xData3[x]
+
+		# compute both the starting and ending (x, y)-coordinates for
+		# the text prediction bounding box
+		endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+		endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+		startX = int(endX - w)
+		startY = int(endY - h)
+
+		# add the bounding box coordinates and probability score to
+		# our respective lists
+		rects.append((startX, startY, endX, endY))
+		confidences.append(scoresData[x])
+		#rects = cv2.groupRectangles(rects, 0, 0.85)
+
+# apply non-maxima suppression to suppress weak, overlapping bounding
+# boxes
+boxes = non_max_suppression(np.array(rects), probs=confidences)
 
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+rectsx1 = []
+rectsy1 = []
+rectsx2 = []
+rectsy2 = []
+c=0
+# loop over the bounding boxes
+for (startX, startY, endX, endY) in boxes:
+	# scale the bounding box coordinates based on the respective
+	# ratios
+
+	startX = int(startX * rW)
+	startY = int(startY * rH) -100
+	endX = int(endX * rW)
+	endY = int(endY * rH)
+
+	# draw the bounding box on the image
+
+	cv2.rectangle(blackimage, (startX, startY), (endX, endY), (0, 255, 0), 2)
 
 
-contours, hierarchy = cv2.findContours(edges,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-total = 0
+edgesbrown = cv2.Canny(blackimage, 10, 250)
+contours, hierarchy = cv2.findContours(edgesbrown,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
+
+xcoordarray = np.zeros([len(contours),2])
+count=0
 for c in contours:
-    # approximate the contour
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.1 * peri, True)
+	area = cv2.contourArea(c)
+	x, y, w, h = cv2.boundingRect(c)
+	#ratio = w/h
+	#print("ratio= " + str(ratio))
+	cv2.drawContours(blackimage, c, -1, (160, 30, 70), 3)
+	x, y, w, h = cv2.boundingRect(c)
+	cv2.rectangle(blackimage, (x, 0), (x + w, 1010), (160, 130, 70), 2)
 
-    # if the approximated contour has four points, then assume that the
-    # contour is a book -- a book is a rectangle and thus has four vertices
-    if len(approx) == 4:
-        #cv2.drawContours(black, [approx], -1, (0, 255, 0), 4)
-        total += 1
-#print(total)
+	xcoordarray[count,0] =x
+	xcoordarray[count,1] =x+w
 
+	count=count+1
 
+	center = (x, y)
 
-#cv2.imshow('Canny Edges After Contouring', closed)
-#cv2.waitKey(0)
+	#cropped = imgclear[y:y + h, x:x + w]
 
-print("Number of Contours found = " + str(len(contours)))
+	#  cv2.imshow("cropped", cropped)
+	# cv2.imwrite( str(x)+".png", cropped)
+	# cv2.waitKey()
 
-# Draw all contours
-# -1 signifies drawing all contours
-cv2.drawContours(black, contours, -1, (0, 255, 0), 3)
+sorted_array = xcoordarray[np.argsort(xcoordarray[:, 0])]
+book_gap =[]
+book_width =[]
 
+print(sorted_array)
+lastx1 = 0
+lastx2 =0
+drawn =0
+for x in range(len(contours)):
+	x1 = int(sorted_array[x][0])-5
+	x2 = int(sorted_array[x][1])+5
+	#print(x1)
 
-#print(contours)
-#cv2.imshow('Contours', black)
+	if abs(lastx1 - x1) > 30 :
+		cv2.rectangle(image, (x1, 0), (x2, 1010), (78, 0, 20), 2)
 
+		cropped = image[0:1010, x1:x2]
+#		cv2.imshow("cropped"+str(drawn), cropped)
+#		cv2.waitKey()
+		if drawn > 0:
+			book_gap.append(x1 - lastx2)
+			book_width.append(x2-x1)
+		drawn = drawn + 1
 
-
-def filter_by_Hough():
-    lines = cv2.HoughLinesP(edges,rho = 1,theta = 1*np.pi/180,threshold = 100,minLineLength = 100,maxLineGap = 10)
-    linecoords, count = fill_array(lines)
-
-
-#    linecoords = get_num_books(linecoords,count)
-
-
-
-    c = 0
-    for line in lines:
-        x1 = linecoords[c][0]
-        y1 = linecoords[c][1]
-        x2 = linecoords[c][2]
-        y2 = linecoords[c][3]
-        c = c + 1
-        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    sorted_array = linecoords[np.argsort(linecoords[:, 0])]
-    print("sorted")
-    print(sorted_array)
-    cv2.imshow("Result Image", img)
-
-    cv2.waitKey()
-
-
-# fills linecoord array with line coords and gradient
-def fill_array(lines):
-    count = 0
-    for line in lines:  # count the number of lines detected
-        count = count + 1
-    linecoord = np.zeros((count, 5), dtype=np.int32)
-    row = 0
-    for line in lines:
-
-        x1, y1, x2, y2 = line[0]
-
-        if abs(x2 - x1) > 0.01:
-            line_grad = (y2 - y1) / (x2 - x1)
-        else:
-            line_grad = 2147483646  # max 32-bit int
-
-
-        linecoord[row][0] = x1
-        linecoord[row][1] = y1
-        linecoord[row][2] = x2
-        linecoord[row][3] = y2
-        linecoord[row][4] = line_grad
-        row = row + 1
-    return linecoord, count
-
-#filter_by_Hough()
-#filter_by_color(black=False)
+	lastx1 = x1
+	lastx2 = x2
 
 
 
 
 
+print(drawn)
+print(book_gap)
+print(book_width)
+book_gap.sort()
+book_gapsmean = statistics.mean(book_gap)
+book_gapstd = statistics.stdev(book_gap)
+book_gapmead = statistics.median(book_gap)
+q1 = statistics.median(book_gap[0:3])
+q3 = statistics.median(book_gap[6:9])
+iqr =q3 - q1
+iqr = 1.5* iqr
+
+q3iqr = q3 + iqr
+q1iqr = q1 - iqr
+
+print(book_gapmead)
+print(book_gapsmean)
+print(book_gapstd)
+
+print(q3iqr)
+print(q1iqr)
+
+for number in book_gap:
+	if q3iqr < number:
+		print("outlier " + str(number))
 
 
-"""""
-def get_num_books(lines,length):
-    for line in lines:
-        print(line+100)
-        if False == True:
-            line[0] = 0
-            line[1] = 0
-            line[2] = 0
-            line[3] = 0
-            line[4] = 0
-    return lines
-"""""
-
-
-
-
-
-
-
-
-"""""
-
-
-def get_lines_sorted_by_n(lines,n):  # return a sorted list of circle center coordiantes sorted by n
-    if lines is not None:
-        return sorted([center[n] for center in lines])
-
-def remove_very_close_parralel_line(lines,count): # if lines (distance betwwn them and gradient is simmilar) less than threshold remove one of them
-    for i in range(count):
-        j=i+1
-        while j < count:
-            if abs(lines[i][0]-lines [j][0]) < 10 and abs(lines[i][4])-abs(lines [j][4]) < 30 :
-                lines[j][0] =0
-                lines[j][2] =0
-            j = j + 1
-    return lines
-
-def remove_very_apart_lines(lines): ## useless lines that appear in the corners
-    return lines
-
-
-def Detect_book_lines(lines): # if close lines gradients are not close then they are not a book
-    return lines
-
-def remove_short_lines(lines):
-    length_threshold = 0;
-    for line in lines:
-        x1 = line[0]
-        y1 = line[1]
-        x2 = line[2]
-        y2 = line[3]
-        line_length = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-      #  if line_length < length_threshold:  # remove line
+#for num in book_gap:
+	#if num > book_gapstd + book_gapsmean
 
 
 
-
-
-"""
+cv2.imshow("color", image)
+cv2.waitKey()
