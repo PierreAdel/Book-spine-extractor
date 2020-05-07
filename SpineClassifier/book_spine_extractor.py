ac9fc08072c5c8a1c953398aefd86703fdea6615
Ramez <rameznoshy@gmail.com>
2020-05-07 15:14:45 +0200

    chore:add images, edit gitingore

--- SpineClassifier/book_spine_extractor.py
+++ SpineClassifier/book_spine_extractor.py
@@ -7,6 +7,7 @@ SHELVES_PATH = 'images/shelves/'
 
 
 class SpineExtractor:
+    x = 0
 
     def __init__(self, image):
         self.image = image
@@ -21,7 +22,7 @@ class SpineExtractor:
         fld = cv2.ximgproc.createFastLineDetector()
         lines = fld.detect(gray)
         mask = fld.drawSegments(mask, lines)
-        cv2.imshow("LSD", mask)
+        # cv2.imshow("LSD", mask)
 
         for i in range(len(lines)):
             for x1, y1, x2, y2 in lines[i]:
@@ -29,9 +30,9 @@ class SpineExtractor:
                         abs(np.sin(np.arctan((y2 - y1) / max((x2 - x1), 0.01))))]
                 if line[-1] > 0.96 and (y2 - y1) > 0.04 * self.image.shape[0]:  # a slant 15 deg & length > 0.25 height
                     filtered_lines.append([*line, line[-1] * (y2 - y1)])
-                    cv2.line(self.image, (x1, y1), (x2, y2), (0, 255, 255), 1)
-                    cv2.line(mask2, (x1, y1), (x2, y2), (0, 255, 255), 1)
-            cv2.imshow('filter1', self.image)
+                    # cv2.line(self.image, (x1, y1), (x2, y2), (0, 255, 255), 1)
+                    # cv2.line(mask2, (x1, y1), (x2, y2), (0, 255, 255), 1)
+            # cv2.imshow('filter1', self.image)
 
         filtered_lines.sort(key=lambda line: line[4])
         for index in range(len(filtered_lines)):
@@ -41,10 +42,10 @@ class SpineExtractor:
                 filtered_lines[index][-1] += filtered_lines[index + i][-1]
                 filtered_lines[index + i][-1] += filtered_lines[index][-1]
                 i += 1
-            mask2 = cv2.putText(mask2, str(int(filtered_lines[index][-1])), (filtered_lines[index][0], filtered_lines[index][1]),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
+            # mask2 = cv2.putText(mask2, str(int(filtered_lines[index][-1])), (filtered_lines[index][0], filtered_lines[index][1]),
+            #                     cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
 
-        cv2.imshow("strengths", mask2)
+        # cv2.imshow("strengths", mask2)
 
         filtered_lines_by_x = []
         last_x = filtered_lines[0][4]
@@ -58,15 +59,15 @@ class SpineExtractor:
             else:
                 x1, y1, x2, y2, avg_x, sine, strength = max_line
                 filtered_lines_by_x.append(max_line)
-                cv2.line(mask2, (x1, y1), (x2, y2), (255, 0, 0), 1)
-                cv2.line(self.image, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                # cv2.line(mask2, (x1, y1), (x2, y2), (255, 0, 0), 1)
+                # cv2.line(self.image, (x1, y1), (x2, y2), (0, 255, 0), 1)
                 max_line = line
             last_x = avg_x
 
         filtered_lines_by_x.append(max_line)
         x1, y1, x2, y2, avg_x, sine, strength = max_line
-        cv2.line(mask2, (x1, y1), (x2, y2), (255, 0, 0), 1)
-        cv2.line(self.image, (x1, y1), (x2, y2), (0, 255, 0), 1)
+        # cv2.line(mask2, (x1, y1), (x2, y2), (255, 0, 0), 1)
+        # cv2.line(self.image, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
         last_line = filtered_lines_by_x[0]
         x_cuts = [0]
@@ -85,8 +86,8 @@ class SpineExtractor:
         x_cuts.append(int(current_x_cut / max(num_lines, 1)))
         x_cuts.append(self.image.shape[1] - 1)  # todo add this to filtered lines
 
-        for cut in x_cuts:
-            cv2.line(self.image, (cut, 0), (cut, self.image.shape[0] - 1), (0, 0, 255), 1)
+        # for cut in x_cuts:
+        #     cv2.line(self.image, (cut, 0), (cut, self.image.shape[0] - 1), (0, 0, 255), 1)
 
         for cut_index in range(1, len(x_cuts)):
             spine = self.image[0:self.image.shape[0] - 1, x_cuts[cut_index - 1]: x_cuts[cut_index]]
@@ -94,13 +95,12 @@ class SpineExtractor:
             spine = cv2.rotate(spine, cv2.ROTATE_90_COUNTERCLOCKWISE)
             self.spines.append(spine)
 
-        x = 0
         for spine in self.spines:
-            cv2.imwrite(f"detected_spines/spine{x}.jpg", spine)
-            x += 1
-        cv2.imshow("strengths_filtered", mask2)
-        cv2.imshow('filter1', self.image)
-        cv2.waitKey()
+            cv2.imwrite(f"detected_spines/spine{SpineExtractor.x}.jpg", spine)
+            SpineExtractor.x += 1
+        # cv2.imshow("strengths_filtered", mask2)
+        # cv2.imshow('filter1', self.image)
+        # cv2.waitKey()
 
 
 def resize_image(im):
