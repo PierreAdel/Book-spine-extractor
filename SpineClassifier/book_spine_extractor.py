4702f8082c43b007324e7a39c1a45af2cd62a5d5
Ramez <rameznoshy@gmail.com>
2020-05-12 18:48:18 +0200

    return book array, add logs

--- SpineClassifier/book_spine_extractor.py
+++ SpineClassifier/book_spine_extractor.py
@@ -23,8 +23,9 @@ class SpineExtractor:
         books = []
         for spine in extractor.spines:
             book_json = process_spine_from_extractor(spine)
+            print(book_json, flush=True)
             books.append(book_json)
-        pass
+        return books
 
     def __init__(self, image):
         self.image = image
