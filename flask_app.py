45340fa8d34c3b52d2c30eeec2af129594fa620c
Ramez <rameznoshy@gmail.com>
2020-05-12 19:54:18 +0200

    write ans to json

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,7 @@ from flask_restful import Resource, Api, reqparse
 from SpineClassifier.text_segmenter import process_spine
 from SpineClassifier.book_spine_extractor import SpineExtractor
 import werkzeug
+import json
 from flask import Flask
 
 app = Flask(__name__)
@@ -20,12 +21,14 @@ class SpineOCR(Resource):
         return process_spine(image_file)
 
 class ShelfOCR(Resource):
+    def get(self):
+        return json.loads('books.json')
     def post(self):
         parse = reqparse.RequestParser()
         parse.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
         args = parse.parse_args()
         image_file = args['file'].read()
-        return SpineExtractor.extract_spines_from_img_str(image_file)
+        json.dumps(SpineExtractor.extract_spines_from_img_str(image_file))
 
 api.add_resource(SpineOCR, '/')
 api.add_resource(ShelfOCR, '/shelf/')
